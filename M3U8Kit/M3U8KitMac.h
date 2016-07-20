4b7074565b4889c717f59303e01675395d29f5d0
Oleksandr Kurylenko <sasha.kurylenko@gmail.com>
2016-07-20 12:51:36 +0300

    Added Mac build support

---
+++ M3U8Kit/M3U8KitMac.h
@@ -0,0 +1,19 @@
+//
+//  M3U8KitMac.h
+//  M3U8KitMac
+//
+//  Created by Oleksandr Kurylenko on 7/15/16.
+//  Copyright © 2016 Allen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for M3U8KitMac.
+FOUNDATION_EXPORT double M3U8KitMacVersionNumber;
+
+//! Project version string for M3U8KitMac.
+FOUNDATION_EXPORT const unsigned char M3U8KitMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M3U8KitMac/PublicHeader.h>
+
+
